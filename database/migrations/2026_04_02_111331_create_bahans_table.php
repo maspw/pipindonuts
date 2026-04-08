ba5f1e89b7cae6e8e50bcf705d88be550588665e
maspw <rpaz1140@gmail.com>
2026-04-08 15:07:37 +0700

    perbaikan modul bahan

--- database/migrations/2026_04_02_111331_create_bahans_table.php
+++ database/migrations/2026_04_02_111331_create_bahans_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('bahan', function (Blueprint $table) {
+        Schema::create('bahans', function (Blueprint $table) {
             $table->id();
             $table->string('nama_bahan');
             $table->string('satuan');
